c5a5a36d3397d2c63f553ed106deea6b82e6b678
vigz <kyle.vigorito@gmail.com>
2020-02-08 16:55:45 -0500

    fixed indentation

--- account/models.py
+++ account/models.py
@@ -13,13 +13,13 @@ class UserManager(DjangoUserManager):
 
     def _create_user(self, email, password, is_staff,
                  is_superuser, **extra_fields):
-    email = self.normalize_email(email)
-    user = self.model(email=email, is_staff=is_staff,
-                      is_active=True, is_superuser=is_superuser,
-                      **extra_fields)
-    user.set_password(password)
-    user.save(using=self._db)
-    return user
+        email = self.normalize_email(email)
+        user = self.model(email=email, is_staff=is_staff,
+                          is_active=True, is_superuser=is_superuser,
+                          **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
     def create_user(self, email, password=None, **extra_fields):
         return self._create_user(email, password, False, False,
