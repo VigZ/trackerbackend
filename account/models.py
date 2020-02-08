9ff0afae2c3e5f67759ea311c2d8c974fb2120ac
vigz <kyle.vigorito@gmail.com>
2020-02-08 16:55:04 -0500

    added user methods, and new object manager

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import UserManager as DjangoUserManager
 from django.utils.translation import ugettext_lazy as _
 from model_utils.models import TimeStampedModel
 
@@ -8,7 +9,27 @@ class Group(models.Model):
     name = models.CharField(_('name'), max_length=30, blank=False)
 
 
-    
+class UserManager(DjangoUserManager):
+
+    def _create_user(self, email, password, is_staff,
+                 is_superuser, **extra_fields):
+    email = self.normalize_email(email)
+    user = self.model(email=email, is_staff=is_staff,
+                      is_active=True, is_superuser=is_superuser,
+                      **extra_fields)
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, False,
+                             **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True,
+                             **extra_fields)
+
+
 class User(TimeStampedModel, AbstractBaseUser):
     email = models.EmailField(
         _('email address'), unique=True,
--- trackerbackend/settings.py
+++ trackerbackend/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'account.apps.AccountConfig',
     'project.apps.ProjectConfig',
-     'rest_framework',
+    'rest_framework',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
