6e346f32a9866bc6e1cd8102bd9991501cad9832
vigz <kyle.vigorito@gmail.com>
2020-02-08 13:21:24 -0500

    Some user model changes

--- account/models.py
+++ account/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import
 from model_utils.models import TimeStampedModel
 
-class User(TimeStampedModel):
+
+class User(TimeStampedModel, AbstractBaseUser):
     email = models.EmailField(
         _('email address'), unique=True,
         error_messages={
@@ -12,3 +14,4 @@ class User(TimeStampedModel):
         help_text=_('Designates whether this user should be treated as '
                     'active. Unselect this instead of deleting accounts.'))
     display_name = models.CharField(_('display name'), max_length=30, blank=False)
+    password = models.CharField(_('password'), max_length=100, blank=False)
