b37482cf0e8f651544ce037fc07ef21e111e7f3c
vigz <kyle.vigorito@gmail.com>
2020-02-11 11:54:18 -0500

    Added colors to tags

--- project/models.py
+++ project/models.py
@@ -6,6 +6,7 @@ from account.models import User
 
 class Tag(models.Model):
     name = models.CharField(_('name'), max_length=30, blank=False)
+    color = models.CharField(_('color'), max_length=256, blank=True)
 
     def __str__(self):
         return self.name
@@ -28,6 +29,6 @@ class Ticket(models.Model):
     description = models.CharField(_('description'), max_length=1000, blank=True)
     attachment = models.FileField()
     tags = models.ManyToManyField(Tag, related_name="ticket_tags")
-    
+
     def __str__(self):
         return self.name
