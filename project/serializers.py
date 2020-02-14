1a7c03b776f4a6e7873e0b2bca53ec0e33c21d07
vigz <kyle.vigorito@gmail.com>
2020-02-13 23:17:35 -0500

    modified project, ticketgrouping, and ticket serializers

--- project/serializers.py
+++ project/serializers.py
@@ -5,9 +5,25 @@ from .models import Project, Ticket, Tag, TicketGrouping, Comment
 from account.models import User
 
 
+class TicketSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ['url', 'name', 'poster', 'assigned_to', 'description',
+        'attachment', 'tags', 'due_date', 'completed_date', 'severity',
+        'priority', 'steps_to_reproduce', 'ticket_grouping']
+
+
+class TicketGroupingSerializer(serializers.HyperlinkedModelSerializer):
+    ticket_set = TicketSerializer(read_only=True, many=True)
+    class Meta:
+        model = TicketGrouping
+        fields = ['url', 'name', 'project', 'resolution_order', 'ticket_set',]
+
+
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
     admins = UserSerializer(many=True, required=False)
     members = UserSerializer(many=True, required=False)
+    ticketgrouping_set = TicketGroupingSerializer(read_only=True, many=True)
 
     def create(self, validated_data):
         name = validated_data.get('project_name')
@@ -19,27 +35,13 @@ class ProjectSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Project
-        fields = ['url', 'project_name', 'owner', 'admins', 'members']
+        fields = ['url', 'project_name', 'owner', 'admins', 'members', 'ticketgrouping_set',]
 
 
 class TagSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Tag
         fields = ['url', 'name']
-
-
-class TicketGroupingSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = TicketGrouping
-        fields = ['url', 'name', 'project', 'resolution_order',]
-
-
-class TicketSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Ticket
-        fields = ['url', 'name', 'poster', 'assigned_to', 'description',
-                 'attachment', 'tags', 'due_date', 'completed_date', 'severity',
-                 'priority', 'steps_to_reproduce', 'ticket_grouping']
 
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
